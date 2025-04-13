2dfce033efafbd8c8e53e3c85d3cf1c015adaf24
ignaciocorrea1 <ig.correar@duocuc.cl>
2025-04-13 00:04:04 -0400

    Update vistas
    - Vista de catalogo
    - Vista de ver producto

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
+from variables import *
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, "pages/index.html", context)
+    return render(request, INDEX_TEMPLATE, context)
+
+def catalogo(request):
+    context = {}
+    return render(request, CATALOGO_TEMPLATE, context)
+
+def producto(request):
+    context = {}
+    return render(request, VER_PRODUCTO_TEMPLATE, context)
---
+++ variables.py
@@ -0,0 +1,4 @@
+# Rutas
+INDEX_TEMPLATE = "pages/index.html"
+CATALOGO_TEMPLATE = "pages/catalogo.html"
+VER_PRODUCTO_TEMPLATE = "pages/verProducto.html"
