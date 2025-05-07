5e5fbb24b58f7d9bd36f76ec3eaf613c799d2df8
ignaciocorrea1 <ig.correar@duocuc.cl>
2025-05-06 20:47:10 -0400

    Update carrito
    Carro desplegado.
    
    ********************************
    - Actualizar contadores.
    - Agregar cantidad por producto.
    - Eliminar y actualizar producto.
    
    Agregar total general.

--- app/static/js/scriptCarroFnc.js
+++ app/static/js/scriptCarroFnc.js
@@ -24,8 +24,14 @@ async function get_producto(id) {
 
 /* Añadir el producto al localStorage */
 function add_producto_LS(producto) {
-    localStorage.setItem("Carrito", JSON.stringify(producto))
-    console.log("Carrito: ", JSON.parse(localStorage.getItem("Carrito")))
+    // Obtencion del carrito actual o se crea uno
+    const carrito = JSON.parse(localStorage.getItem("Carrito")) || [];
+
+    // Se agrega el producto al carrito
+    carrito.push(producto);
+    
+    // Se setea en el localStorage el carrito
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
 };
 
 /* Evento para obtener y enviar las ids a get_producto */
@@ -36,7 +42,6 @@ $addCarroBtns.forEach(btn => {
         
         /* Se realiza un get a la api */
         const producto = await get_producto(id);
-        console.log("Producto guardado en el localStorage: ", producto);
 
         /* Se añade el producto al localStorage */
         add_producto_LS(producto);
