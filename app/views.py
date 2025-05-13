e3c4f9cc80656eeaa1e55649a4608d7ab876c4e1
ignaciocorrea1 <ig.correar@duocuc.cl>
2025-05-12 21:40:23 -0400

    UPD divisas
    - Cambio de precios en divisas.
    
    ***********************************
    - Controlar mejor los clicks en el boton
    - Actualización del precio en el carrito al aumentar o disminuir las cantidades

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import requests
 from paths import *
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotAllowed
 
 # Create your views here.
 def index(request):
@@ -36,3 +36,57 @@ def catalogo(request):
 def producto(request):
     context = {}
     return render(request, VER_PRODUCTO_TEMPLATE, context)
+
+def cambiar_divisa(request):
+    if request.method == "POST":
+        divisa = request.POST.get("divisa")
+        
+        urlGET = RUTA_API + "valorDolar/"
+        
+        if divisa == "usd":
+            try:
+                # Obtencion del valor del dolar
+                serie = requests.get(urlGET).json()
+                valor_dolar = serie["valor"]
+                
+                print(f"Enviando PUT a: {RUTA_API}actualizarUSD/")
+                
+                # Solicitud put para actualizar los precios en la bd
+                upd_request = requests.put(
+                    f"{RUTA_API}actualizarUSD/",
+                    json={"valor": valor_dolar},
+                    headers={'Content-Type': 'application/json'}
+                )
+                upd_request.raise_for_status()
+
+                return redirect("catalogo")
+                
+            except Exception as e:
+                print("Error en cambiar divisa: ", str(e))
+                # Redirigir a catálogo incluso si hay error
+                return redirect("catalogo")
+        else:
+            try:
+                # Obtencion del valor del dolar
+                serie = requests.get(urlGET).json()
+                valor_dolar = serie["valor"]
+                
+                print(f"Enviando PUT a: {RUTA_API}actualizarCLP/")
+                
+                # Solicitud put para actualizar los precios en la bd
+                upd_request = requests.put(
+                    f"{RUTA_API}actualizarCLP/",
+                    json={"valor": valor_dolar},
+                    headers={'Content-Type': 'application/json'}
+                )
+                upd_request.raise_for_status()
+
+                return redirect("catalogo")
+                
+            except Exception as e:
+                print("Error en cambiar divisa: ", str(e))
+                # Redirigir a catálogo incluso si hay error
+                return redirect("catalogo")
+                
+    else:
+        return HttpResponseNotAllowed(["POST"])
